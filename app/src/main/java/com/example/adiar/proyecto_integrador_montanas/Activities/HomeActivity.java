28a163156f7369df56de358930959bb1adb979a0
steve <steven.ros.cap@gmail.com>
2019-02-07 10:29:34 +0100

    Prueba Log In y Registro

---
+++ app/src/main/java/com/example/adiar/proyecto_integrador_montanas/Activities/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.adiar.proyecto_integrador_montanas.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.adiar.proyecto_integrador_montanas.R;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
