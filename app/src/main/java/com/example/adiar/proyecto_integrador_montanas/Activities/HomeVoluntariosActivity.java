5d156c6ba9d05a1733b1b7d5e399e7a8a16b1204
Alvaro Doncel <adoncel98@gmail.com>
2019-02-08 09:37:02 +0100

    Controlados Toast repetidos. Organizados los layouts. Botones cambian de color al ser pulsados. Revisadas las excepciones. Añadido HomeVoluntarios Añadido el import

--- app/src/main/java/com/example/adiar/proyecto_integrador_montanas/Activities/HomeVoluntariosActivity.java
+++ app/src/main/java/com/example/adiar/proyecto_integrador_montanas/Activities/HomeVoluntariosActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
+
+import com.example.adiar.proyecto_integrador_montanas.R;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
