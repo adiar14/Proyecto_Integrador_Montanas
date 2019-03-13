8c46fc941694f9e68861773be92e6145f8798ab9
steve <steven.ros.cap@gmail.com>
2019-03-13 11:50:26 +0100

    nada

--- app/src/main/java/com/naturex/adiar/proyecto_integrador_montanas/db/RutasSQLiteHelper.java
+++ app/src/main/java/com/naturex/adiar/proyecto_integrador_montanas/db/RutasSQLiteHelper.java
@@ -21,7 +21,7 @@ public class RutasSQLiteHelper extends SQLiteOpenHelper {
                     RutaContract.RutaEntry.COLUMN_LONGITUD+" DOUBLE NOT NULL UNIQUE," +
                     RutaContract.RutaEntry.COLUMN_PENDIENTE+" INTEGER NOT NULL UNIQUE," +
                     RutaContract.RutaEntry.COLUMN_DIFICULTAD+" TEXT NOT NULL, " +
-                    RutaContract.RutaEntry.COLUMN_IMAGEN+" TEXT NOT NULL, " +
+                    RutaContract.RutaEntry.COLUMN_IMAGEN+" TEXT NOT NULL," +
                     RutaContract.RutaEntry.COLUMN_SUCIEDAD+" TEXT NOT NULL);" ;
 
     public RutasSQLiteHelper(Context context) {
@@ -44,11 +44,11 @@ public class RutasSQLiteHelper extends SQLiteOpenHelper {
 
     private void rellenarArray() {
 
-        cargaInicial.add(new Ruta("Pico de Abantos 1.763 m - desde San Lorenzo del Escorial (Monte Abantos)",  9.09, 709, "Fácil","img1", "Bajo"));
-        cargaInicial.add(new Ruta("Frente del Agua - Guerra Civil Española - Paredes de Buitrago  ",  16.19, 336, "Fácil","img2", "Medio"));
-        cargaInicial.add(new Ruta("Patones de Arriba al Cancho de la Cabeza - Circular",  11.06, 498, "Fácil","img3", "Bajo"));
-        cargaInicial.add(new Ruta("Cuerda Larga del Puerto Navacerrada al Puerto la Morcuera - (Sierra de Guadarrama)  ",  20.64, 1035, "Medio","img4", "Bajo"));
-        cargaInicial.add(new Ruta("Cercedilla a Bola del Mundo con Vivac, por las Cabrillas en la Barranca y vuelta por Siete Picos, Puricelli ",  37.98, 1806, "Difíci","img5", "Alto"));
+        cargaInicial.add(new Ruta("Pico de Abantos 1.763 m - desde San Lorenzo del Escorial (Monte Abantos)",  9.09, 709, "Fácil","img1.jpg", "Bajo"));
+        cargaInicial.add(new Ruta("Frente del Agua - Guerra Civil Española - Paredes de Buitrago  ",  16.19, 336, "Fácil","img2.jpg", "Medio"));
+        cargaInicial.add(new Ruta("Patones de Arriba al Cancho de la Cabeza - Circular",  11.06, 498, "Fácil","img3.jpg", "Bajo"));
+        cargaInicial.add(new Ruta("Cuerda Larga del Puerto Navacerrada al Puerto la Morcuera - (Sierra de Guadarrama)  ",  20.64, 1035, "Medio","img4.jpg", "Bajo"));
+        cargaInicial.add(new Ruta("Cercedilla a Bola del Mundo con Vivac, por las Cabrillas en la Barranca y vuelta por Siete Picos, Puricelli ",  37.98, 1806, "Difíci","img5.jpg", "Alto"));
     }
 
     private void cargaDatosInicial(SQLiteDatabase db) {
