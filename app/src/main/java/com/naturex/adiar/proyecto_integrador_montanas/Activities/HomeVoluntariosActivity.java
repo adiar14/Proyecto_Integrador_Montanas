b15343e17ec858f0debb09f016092b86d06fb770
steve <steven.ros.cap@gmail.com>
2019-03-15 11:39:42 +0100

    3-Modificacion del voluntarios

--- app/src/main/java/com/naturex/adiar/proyecto_integrador_montanas/Activities/HomeVoluntariosActivity.java
+++ app/src/main/java/com/naturex/adiar/proyecto_integrador_montanas/Activities/HomeVoluntariosActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 
@@ -12,6 +14,10 @@ import com.bumptech.glide.Glide;
 import com.naturex.adiar.proyecto_integrador_montanas.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.naturex.adiar.proyecto_integrador_montanas.adapters.AdapterRutas;
+import com.naturex.adiar.proyecto_integrador_montanas.db.RutaDataSource;
+import com.naturex.adiar.proyecto_integrador_montanas.javabeans.Ruta;
+import com.naturex.adiar.proyecto_integrador_montanas.utilities.ConexionSQLiteHelper;
 
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -23,12 +29,20 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class HomeVoluntariosActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     FirebaseUser usuarioActual;
     private FirebaseAuth mAuth;
 
+    private LinearLayoutManager lr;
+    ArrayList<Ruta> listaRutas;
+    RecyclerView recyclerViewRuta;
+    private AdapterRutas adapter;
+    RutaDataSource rms;
+    ConexionSQLiteHelper conn;
 
     TextView navNombreUsuario;
     TextView navUsuarioEmail;
@@ -40,6 +54,18 @@ public class HomeVoluntariosActivity extends AppCompatActivity
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Carga del reciclerView con los datos almacenados
+        listaRutas = new ArrayList<>();
+        rms = new RutaDataSource(this);
+        listaRutas=rms.consultarRuta();
+        recyclerViewRuta = findViewById(R.id.rvRutas);
+        lr = new LinearLayoutManager(this);
+        recyclerViewRuta.setLayoutManager(lr);
+        adapter = new AdapterRutas(listaRutas);
+        recyclerViewRuta.setAdapter(adapter);
+        recyclerViewRuta.setHasFixedSize(true);
+
 
         //Init Firebase
         mAuth = FirebaseAuth.getInstance();
